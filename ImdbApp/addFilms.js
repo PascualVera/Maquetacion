d79edd51a1f43c1c4b36bd8d9f84b9666540b29d
PascualVera <pascualvera.994@gmail.com>
2022-02-09 17:56:22 +0100

    Responsive checked, todavia hay un bug en el dropdown

--- ImdbApp/addFilms.js
+++ ImdbApp/addFilms.js
@@ -1,49 +1,63 @@
-import { Movie } from "./movie.js";
-import { IMDB } from "./imdb.js";
+import { Movie } from "./movie.js"
+import { IMDB } from "./imdb.js"
 
 //Variables
-let nombre = document.getElementById("nombre");
-let año = document.getElementById("año");
-let pais = document.getElementById("pais");
-let foto = document.getElementById("foto");
-let actores = document.getElementById("actores");
-let director = document.getElementById("director");
-let escritor = document.getElementById("escritor");
-let idioma = document.getElementById("lenguaje");
-let prota = document.getElementById("protagonista");
-let productor = document.getElementById("productor");
-let distribuidor = document.getElementById("distribuidor");
-let genero = document.getElementById("genero");
-let platform = document.getElementById("platform");
-let boton = document.getElementById("boton");
+let nombre = document.getElementById("nombre")
+let año = document.getElementById("año")
+let pais = document.getElementById("pais")
+let foto = document.getElementById("foto")
+let actores = document.getElementById("actores")
+let director = document.getElementById("director")
+let escritor = document.getElementById("escritor")
+let idioma = document.getElementById("lenguaje")
+let prota = document.getElementById("protagonista")
+let productor = document.getElementById("productor")
+let distribuidor = document.getElementById("distribuidor")
+let genero = document.getElementById("genero")
+let platform = document.getElementById("platform")
 
-let filmData = document.getElementById("film_database");
-let filmItem = document.getElementById("film_items");
-let database = new IMDB();
+let filmData = document.getElementById("film_database")
+let filmItem = document.getElementById("film_items")
+let database = new IMDB()
 boton.addEventListener("click", function () {
-  database.peliculas.push(
-    new Movie(
-      nombre.value,
-      año.value,
-      pais.value,
-      foto.value,
-      genero.value,
-      director.value,
-      escritor.value,
-      idioma.value,
-      platform.value,
-      prota.value,
-      productor.value,
-      distribuidor.value
-    )
-  );
+	database.peliculas.push(
+		new Movie(
+			nombre.value,
+			año.value,
+			pais.value,
 
-  for (let i = 0; i < database.peliculas.length; i++) {
-    filmData.innerHTML += `<div class="film_items index${i}"></div>`;
-    for (const att in database.peliculas[i]) {
-      document.getElementsByClassName(
-        `index${i}`
-      ).innerHTML += `<span> ${att}: ${database.peliculas[i][att]}</span>`;
-    }
-  }
-});
+			genero.value,
+			director.value,
+			escritor.value,
+			idioma.value,
+			platform.value,
+			prota.value,
+			productor.value,
+			distribuidor.value,
+			actores.value
+		)
+	)
+
+	filmData.innerHTML = ""
+	for (const film of database.peliculas) {
+		film.foto = foto.value
+		filmData.innerHTML += `<div class=film_items>
+    <figure class=imgFilm_container><img class=imgFilm src=${film.foto}></figure>
+
+
+
+    <p> Título: ${film.title}</p>
+    <p> Lanzamiento: ${film.releaseYear}</p>
+    <p> País: ${film.nationality}</p>
+    <p> Género: ${film.genre}</p>
+    <p> Director: ${film.director}</p>
+    <p> Escritor: ${film.writer}</p>
+    <p> Idioma: ${film.language}</p>
+    <p> Protagonista: ${film.mainCharacterName}</p>
+    <p> Productor: ${film.producer}</p>
+    <p> Distribuidor: ${film.distributor}</p>
+    <p> Reparto: ${film.actors}</p>
+    </div>
+   `
+	}
+})
--- ImdbApp/movie.js
+++ ImdbApp/movie.js
@@ -1,48 +1,51 @@
 export class Movie {
-  //Atributos
+	//Atributos
 
-  //Constructor
-  constructor(
-    title,
-    year,
-    pais,
-    genero,
-    fot,
-    director,
-    escritor,
-    idioma,
-    platform,
-    main,
-    producer,
-    distribuidor
-  ) {
-    this.title = title;
-    this.releaseYear = year;
-    this.nationality = pais;
-    this.genre = genero;
-    this.foto = fot;
-    this.director = director;
-    this.writer = escritor;
-    this.language = idioma;
-    this.platform = platform;
-    this.mainCharacterName = main;
-    this.producer = producer;
-    this.distributor = distribuidor;
-  }
-  //Public metods
-  getStringActors() {
-    let actores = "";
-    for (let i = 0; i < this.actors.length; i++) {
-      if (i == this.actors.length - 1) {
-        actores += this.actors[i].name + ".";
-      } else {
-        actores += this.actors[i].name + ", ";
-      }
-    }
-    return actores;
-  }
-  printAll() {
-    console.log(`Título: ${this.title}
+	//Constructor
+	constructor(
+		title,
+		year,
+		pais,
+		genero,
+
+		director,
+		escritor,
+		idioma,
+		platform,
+		main,
+		producer,
+		distribuidor,
+		actores
+	) {
+		this.title = title
+		this.releaseYear = year
+		this.nationality = pais
+		this.genre = genero
+
+		this.director = director
+		this.writer = escritor
+		this.language = idioma
+		this.platform = platform
+		this.mainCharacterName = main
+		this.producer = producer
+		this.distributor = distribuidor
+		this.actors = actores
+		this.foto = foto
+	}
+	//Public metods
+	getStringActors() {
+		let actores = ""
+		for (let i = 0; i < this.actors.length; i++) {
+			if (i == this.actors.length - 1) {
+				actores += this.actors[i].name + "."
+			} else {
+				actores += this.actors[i].name + ", "
+			}
+		}
+		return actores
+	}
+	printAll() {
+		console.log(`Título: ${this.title}
 Año de lanzamiento: ${this.releaseYear}
 Actores: ${this.getStringActors()}
 País: ${this.nationality}
@@ -54,6 +57,6 @@ Universo Marvel: ${this.isMCU}
 Protagonista: ${this.mainCharacterName}
 Productor: ${this.producer.name}
 Distribuidora: ${this.distributor}
-Género: ${this.genre}`);
-  }
+Género: ${this.genre}`)
+	}
 }
